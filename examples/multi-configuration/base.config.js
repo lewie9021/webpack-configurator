9ac10d1a6e0d641f71432446110c1e0000d0c7e4
Lewis Barnes <lb.barnes@live.co.uk>
2016-02-09 20:56:03 +0000

    Split the base config into 'config' and 'loaders'. - This is to prevent applying the invalid 'loaders' property in the final merge.

--- examples/multi-configuration/base.config.js
+++ examples/multi-configuration/base.config.js
@@ -6,16 +6,20 @@ var srcPath = Path.join(__dirname, "src");
 var outputPath = Path.join(__dirname, "dist");
 
 module.exports = {
-    entry: Path.join(__dirname, "src", "index.js"),
-    output: {
-        path: outputPath,
-        filename: "bundle.js",
-        publicPath: "/static/"
+    // Generic Webpack configuration.
+    config: {
+        entry: Path.join(__dirname, "src", "index.js"),
+        output: {
+            path: outputPath,
+            filename: "bundle.js",
+            publicPath: "/static/"
+        },
+        resolve: {
+            root: srcPath,
+            extensions: ["", ".jsx", ".js"]
+        },
     },
-    resolve: {
-        root: srcPath,
-        extensions: ["", ".jsx", ".js"]
-    },
+    // Our basic loaders.
     loaders: [
         // Enable ES6 & JSX syntax.
         loader({
