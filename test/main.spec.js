969ce345059edcfb7be969235ee7529bc209f8bc
Lewis Barnes <lb.barnes@live.co.uk>
2016-02-14 13:16:44 +0000

    Added a main spec file that covers the top level API.

---
+++ test/main.spec.js
@@ -0,0 +1,102 @@
+var expect = require("chai").expect;
+var Config = require("../");
+
+describe("Top-Level Exports:", function() {
+
+    describe("loader", function() {
+
+        it("is a function", function() {
+
+        });
+
+        it("accepts a single object as a parameter", function() {
+
+        });
+
+        it("returns a loader object", function() {
+
+        });
+
+    });
+
+    describe("loaders", function() {
+
+        it("is a function", function() {
+
+        });
+
+        it("accepts an array of loader configurations", function() {
+
+        });
+
+        it("calls the loader function for each configuration", function() {
+
+        });
+
+        it("returns an array of loader objects", function() {
+
+        });
+
+    });
+
+    describe("plugin", function() {
+
+        it("is a function", function() {
+
+        });
+
+        it("accepts a single object as a parameter", function() {
+
+        });
+
+        it("returns a plugin object", function() {
+
+        });
+
+    });
+
+    describe("plugins", function() {
+
+        it("is a function", function() {
+
+        });
+
+        it("accepts an array of plugin configurations", function() {
+
+        });
+
+        it("calls the plugin function for each configuration", function() {
+
+        });
+
+        it("returns an array of plugin objects", function() {
+
+        });
+
+    });
+
+    describe("utilities", function() {
+
+        it("is an object", function() {
+
+        });
+
+        it("exposes 'merge' and 'resolveAll'", function() {
+
+        });
+
+    });
+
+    describe("helpers", function() {
+
+        it("is an object", function() {
+
+        });
+
+        it("exposes 'concatMerge'", function() {
+
+        });
+
+    });
+
+});
