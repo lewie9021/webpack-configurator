bae0e432588f31e398e225a94ca4084380fe020d
Lewis Barnes <lb.barnes@live.co.uk>
2016-02-15 22:53:41 +0000

    Initial stab at specs for the merge function. - TODO: Add specs to cover parameter validation.

--- test/loader.spec.js
+++ test/loader.spec.js
@@ -5,6 +5,26 @@ describe("Loader:", function() {
 
     describe("merge", function() {
 
+        it("accepts an object to merge with the current loader config", function() {
+
+        });
+
+        it("accepts a function who's return value is merged with the current loader config", function() {
+
+        });
+
+        it("accepts a property string to allow direct merging on top-level properties", function() {
+
+        });
+
+        it("accepts a customizer function to allow behaviour tweaking during the merge", function() {
+
+        });
+
+        it("returns the loader object to allow chaining", function() {
+
+        });
+
     });
 
     describe("set", function() {
