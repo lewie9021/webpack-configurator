fc66fba94ce42edba92a178fbd4eb8128cb751c6
Lewis Barnes <lb.barnes@live.co.uk>
2016-02-17 20:18:40 +0000

    Added a types helper to make type validation easier.

---
+++ test/helpers/types.js
@@ -0,0 +1,13 @@
+module.exports = function() {
+    return {
+        string: "test",
+        int: 5,
+        decimal: 4.3,
+        regex: /[A-Z]+/,
+        array: [],
+        object: {},
+        null: null,
+        func: function() {},
+        boolean: true
+    };
+};
