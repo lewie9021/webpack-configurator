f6d290615cce9533a437f427158078166ff9d34f
Lewis Barnes <lb.barnes@live.co.uk>
2016-02-21 16:33:17 +0000

    Added specs for Utilities.resolveAll.

--- test/utilities.spec.js
+++ test/utilities.spec.js
@@ -1,10 +1,88 @@
+var Chai = require("chai");
+var Sinon = require("sinon");
+var Types = require("./helpers/types");
+var Utilities = require("../lib/utilities");
+
+var expect = Chai.expect;
+var types = Types();
+
 describe("Utilities:", function() {
+
+    beforeEach(function() {
+        this.sandbox = Sinon.sandbox.create();
+    });
+
+    afterEach(function() {
+        this.sandbox.restore();
+    });
 
     describe("merge", function() {
 
     });
 
     describe("resolveAll", function() {
+
+        it("is a function", function() {
+            expect(Utilities.resolveAll).to.be.a("function");
+        });
+
+        it("accepts an array of values that contain a 'resolve' property", function() {
+            var loader = {
+                resolve: function() {
+                    return "loader";
+                }
+            };
+            var plugin = {
+                resolve: function() {
+                    return "plugin";
+                }
+            };
+
+            var actual = Utilities.resolveAll([loader, plugin]);
+            var expected = ["loader", "plugin"];
+
+            expect(actual).to.eql(expected);
+        });
+
+        it("throws if an array isn't passed", function() {
+            var error = "";
+
+            Object.keys(types).forEach(function(type) {
+                if (type == "array")
+                    return;
+
+                expect(function() {
+                    Utilities.resolveAll(types[type]);
+                }).to.throw(error);
+            });
+        });
+
+        it("throws if values in the passed array don't contain a resolve function", function() {
+            var error = "";
+
+            Object.keys(types).forEach(function(type) {
+                if (type == "array")
+                    return;
+
+                expect(function() {
+                    Utilities.resolveAll([types[type]]);
+                }).to.throw(error);
+            });
+        });
+
+        it("calls the 'resolve' property of object passed", function() {
+            var loader = {
+                resolve: this.sandbox.spy()
+            };
+            var plugin = {
+                resolve: this.sandbox.spy()
+            };
+
+            Utilities.resolveAll([loader, plugin]);
+
+            expect(loader.resolve.callCount).to.eq(1);
+            expect(plugin.resolve.callCount).to.eq(1);
+        });
 
     });
 
