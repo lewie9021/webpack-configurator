4283eaadd459ce2cd9a3bb8cf773402d9d625f25
Lewis Barnes <lb.barnes@live.co.uk>
2016-02-21 22:27:49 +0000

    Added outline specs for merge and set.

--- test/plugin.spec.js
+++ test/plugin.spec.js
@@ -8,9 +8,53 @@ describe("Plugin:", function() {
 
     describe("merge", function() {
 
+        it("merges parameters, given an array for 'changes'", function() {
+
+        });
+
+        it("merges parameters at specific indexes, given a 'changes' object with numerical keys", function() {
+
+        });
+
+        it("throws if a 'changes' object is passed with non-numerical keys", function() {
+
+        });
+
+        it("throws if 'changes' isn't an object or array", function() {
+
+        });
+
+        it("merges a parameter at a specific index, given an 'index' integer", function() {
+
+        });
+
+        it("throws if 'index' isn't an integer", function() {
+
+        });
+
     });
 
     describe("set", function() {
+
+        it("overwrites parameters, given an array for 'changes'", function() {
+
+        });
+
+        it("overwrites parameters at specific indexes, given a 'changes' object with numerical keys", function() {
+
+        });
+
+        it("throws if a 'changes' object is passed with non-numerical keys", function() {
+
+        });
+
+        it("overwrites a parameter at a specific index, given an 'index' integer", function() {
+
+        });
+
+        it("throws if 'index' isn't an integer", function() {
+
+        });
 
     });
 
