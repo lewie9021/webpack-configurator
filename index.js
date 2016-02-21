3e949a528667776cec11d7a975876112aebbc3f4
Lewis Barnes <lb.barnes@live.co.uk>
2016-02-21 20:44:31 +0000

    Properties in Config.utilties now points to the correct functions.

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
 var Loader = require("./lib/loader");
 var Loaders = require("./lib/loaders");
+var Utilities = require("./lib/utilities");
 var Helpers = require("./lib/helpers");
-
-function merge() {
-
-}
-
-function resolveAll() {
-
-}
 
 module.exports = {
     loader: Loader,
     loaders: Loaders,
-    utilities: {
-        merge: merge,
-        resolveAll: resolveAll
-    },
+    utilities: Utilities,
     helpers: Helpers
 };
