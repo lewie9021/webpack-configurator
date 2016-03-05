6634478726af8d5e9007d0b4078280c9d6c1ef32
Lewis Barnes <lb.barnes@live.co.uk>
2016-03-05 21:50:52 +0000

    Added a spec for the multi-configuration dev example. - TODO: Allow strings passed to 'include'.

---
+++ test/examples.spec.js
@@ -0,0 +1,52 @@
+var Path = require("path");
+var Webpack = require("webpack");
+var Chai = require("chai");
+
+var expect = Chai.expect;
+
+describe("Examples:", function() {
+
+    describe("multi-configuration", function() {
+
+        xit("correctly resolves the dev configuration", function() {
+            var rootPath = Path.join(__dirname, "..", "examples", "multi-configuration");
+            var srcPath = Path.join(rootPath, "src");
+            var outputPath = Path.join(rootPath, "dist");
+
+            var actual = require("../examples/multi-configuration/dev.config");
+            var expected = {
+                devtool: "source-map",
+                entry: [
+                    "webpack-dev-server/client?http://localhost:3000",
+                    Path.join(srcPath, "index.js")
+                ],
+                watch: true,
+                output: {
+                    path: outputPath,
+                    filename: "bundle.js",
+                    publicPath: "/static/"
+                },
+                resolve: {
+                    root: srcPath,
+                    extensions: ["", ".jsx", ".js"]
+                },
+                module: {
+                    loaders: [
+                        {
+                            test: /\.jsx?$/,
+                            loader: "babel",
+                            include: srcPath
+                        }
+                    ]
+                },
+                plugins: [
+                    new Webpack.HotModuleReplacementPlugin()
+                ]
+            };
+
+            expect(actual).to.eql(expected);
+        });
+
+    });
+
+});
